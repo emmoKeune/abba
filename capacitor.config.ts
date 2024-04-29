104075f816931836b5ca3ec1795622307ad63713
emmoKeune <99411780+emmoKeune@users.noreply.github.com>
2024-04-29 20:31:58 -0300

    Add existing project files to Git

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.gov.pr.celepar.pocbiometria',
+  appName: 'poc-bio',
+  webDir: 'www'
+};
+
+export default config;
